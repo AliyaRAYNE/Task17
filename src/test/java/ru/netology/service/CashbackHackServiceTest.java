7fd67d3563ba0b3a575b2f01befad5b54ef150b0
Moon <Aliya0609@mail.ru>
2023-05-20 02:52:39 +0500

    Testng added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,49 @@
+package ru.netology.service;
+
+public class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void PurchaseAmountFor500() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 500;
+        int expected = 500;
+        int actual = service.remain(amount);
+        org.testng.Assert.assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void PurchaseAmountFor1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        org.testng.Assert.assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void PurchaseAmountFor2000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 2000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        org.testng.Assert.assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void PurchaseAmountForZero() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int expected = 1000;
+        int actual = service.remain(amount);
+        org.testng.Assert.assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void PurchaseAmountFor999() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int expected = 1;
+        int actual = service.remain(amount);
+        org.testng.Assert.assertEquals(actual, expected);
+    }
+}
